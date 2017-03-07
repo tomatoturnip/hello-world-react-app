142ebd4d2aab100e0a5c44306ae1f1104ec4489d
Lydia Ho <lyd.w.ho@gmail.com>
2017-03-07 14:25:23 -0800

    basic hello world react app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom"; // tells Javascript that we want the ReactDOM library from the react-dom NPM module
+import App from "./App"; // tells Javascript we want to import the App component from a local file called App.js
+import "./index.css";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render( <App />, document.getElementById("root") );
